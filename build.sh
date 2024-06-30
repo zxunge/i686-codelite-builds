41e6f6759ee944726ce12379264bfd9289aef429
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-30 11:47:59 +0800

    Initial trying.

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+set -eux
+
+BUILD_DATE=20240630
+NAME=CodeLite-build${BUILD_DATE}
+HOME=$(cygpath -m ~)
+
+echo 'export PATH=/clang32/bin:$PATH' >> ~/.$(basename $SHELL)rc
+. ~/.$(basename $SHELL)rc
+
+git clone https://github.com/wxWidgets/wxWidgets
+cd wxWidgets
+git submodule update --init
+
+mkdir build-release
+cd build-release
+cmake .. -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release                 \
+         -DwxBUILD_DEBUG_LEVEL=0                                        \
+         -DwxBUILD_MONOLITHIC=1 -DwxBUILD_SAMPLES=SOME -DwxUSE_STL=1    \
+         -DCMAKE_INSTALL_PREFIX=$HOME/root
+mingw32-make -j$(nproc)
+
+git clone https://github.com/eranif/wx-config-msys2.git
+cd wx-config-msys2
+mkdir build-release
+cd $_
+cmake .. -DCMAKE_BUILD_TYPE=Release -G"MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/root"
+mingw32-make -j$(nproc) install
+
+export PATH=~/root/bin/:$PATH
+export MSYS2_BASE=/D/msys64
+
+git clone https://github.com/eranif/codelite.git
+cd codelite
+git submodule update --init --recursive
+mkdir build-release
+cd $_
+cmake .. -DCMAKE_BUILD_TYPE=Release -G"MinGW Makefiles" -DWXWIN="$HOME/root" DCMAKE_INSTALL_PREFIX=~/codelite -Wno-dev
+mingw32-make -j$(nproc) install
+
+7zr a -mx9 -mqs=on -mmt=on ~/${NAME}.7z ~/codelite
+
+if [[ -v GITHUB_WORKFLOW ]]; then
+  echo "OUTPUT_BINARY=${HOME}/${NAME}.7z" >> $GITHUB_OUTPUT
+  echo "RELEASE_NAME=${NAME}" >> $GITHUB_OUTPUT
+  echo "BUILD_DATE=${BUILD_DATE}" >> $GITHUB_OUTPUT
+  echo "OUTPUT_NAME=${NAME}.7z" >> $GITHUB_OUTPUT
+fi
+
